6cb31ce6b20fd9fe3951f899e151c0a407e624a4
Arturo Castillo Valles <acv629@gmail.com>
2018-03-23 19:58:38 -0600

    Definida operación crear
    Definida operación para crear usuarios.
    Documentada clase y operación nueva.

--- src/main/java/mx/ciencias/is/capisoft/modelo/dao/UsuarioDAO.java
+++ src/main/java/mx/ciencias/is/capisoft/modelo/dao/UsuarioDAO.java
@@ -6,12 +6,43 @@
 package mx.ciencias.is.capisoft.modelo.dao;
 
 import mx.ciencias.is.capisoft.modelo.HibernateUtil;
+import mx.ciencias.is.capisoft.modelo.Usuario;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 /**
+ * Data Access Object para la entidad Usuario
  *
  * @author acv629
+ * @version 1.0
  */
 public class UsuarioDAO {
-    private final SessionFactory session = HibernateUtil.getSessionFactory();
+
+  private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+  /**
+   * Añade un nuevo usuario
+   *
+   * @param usuarioNuevo El usuario a añadir a la BD
+   */
+  public void crear(Usuario usuarioNuevo) {
+    Session session = sessionFactory.openSession();
+    Transaction tx = session.beginTransaction();
+    try {
+      tx.begin();
+
+      session.persist(usuarioNuevo);
+
+      tx.commit();
+
+    } catch (HibernateException e) {
+      if (tx != null) {
+        tx.rollback();
+      }
+      e.printStackTrace();
+    }
+
+  }
 }
