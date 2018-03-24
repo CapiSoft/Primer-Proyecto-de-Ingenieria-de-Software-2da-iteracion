c41123ac20d2651db4ebf00d5d9984447c4eb379
Arturo Castillo Valles <acv629@gmail.com>
2018-03-24 14:49:10 -0600

    Añadida operación para añadir comentarios

--- src/main/java/mx/ciencias/is/capisoft/modelo/dao/ComentarioDAO.java
+++ src/main/java/mx/ciencias/is/capisoft/modelo/dao/ComentarioDAO.java
@@ -5,8 +5,12 @@
  */
 package mx.ciencias.is.capisoft.modelo.dao;
 
+import mx.ciencias.is.capisoft.modelo.Comentario;
 import mx.ciencias.is.capisoft.modelo.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 /**
  * Data Access Object para la entidad Comentario
@@ -17,5 +21,29 @@ import org.hibernate.SessionFactory;
 public class ComentarioDAO {
 
   private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+  /**
+   * Añade un nuevo comentario
+   *
+   * @param comentarioNuevo El comentario a añadir a la BD
+   */
+  public void crear(Comentario comentarioNuevo) {
+    Session session = sessionFactory.openSession();
+    Transaction tx = session.beginTransaction();
+    try {
+      tx.begin();
+
+      session.persist(comentarioNuevo);
+
+      tx.commit();
+    } catch (HibernateException e) {
+      if (tx != null) {
+        tx.rollback();
+      }
+      e.printStackTrace();
+    } finally {
+      session.close();
+    }
+  }
 
 }
