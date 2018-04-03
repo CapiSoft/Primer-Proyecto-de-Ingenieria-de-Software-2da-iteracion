640bc2b7b01d9b9e238108f0f12e5b1bf082cab3
Arturo Castillo Valles <acv629@gmail.com>
2018-04-02 23:33:14 -0500

    Implementado casos de uso
    Casos de uso Leer Pregunta y Leer Comentario

--- src/main/java/mx/ciencias/is/capisoft/controlador/LeerPregunta.java
+++ src/main/java/mx/ciencias/is/capisoft/controlador/LeerPregunta.java
@@ -5,8 +5,14 @@
  */
 package mx.ciencias.is.capisoft.controlador;
 
+import java.io.Serializable;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import mx.ciencias.is.capisoft.modelo.Comentario;
+import mx.ciencias.is.capisoft.modelo.Pregunta;
+import mx.ciencias.is.capisoft.modelo.dao.ComentarioDAO;
+import mx.ciencias.is.capisoft.modelo.dao.PreguntaDAO;
 
 /**
  *
@@ -14,6 +20,41 @@ import javax.faces.bean.ViewScoped;
  */
 @ManagedBean
 @ViewScoped
-public class LeerPregunta {
+public class LeerPregunta implements Serializable {
+
+  private Pregunta preguntaPedida = new Pregunta();
+  private List<Comentario> comentariosPregunta;
+
+  public List<Comentario> getComentariosPregunta() {
+    return comentariosPregunta;
+  }
+
+  public void setComentariosPregunta(List<Comentario> comentariosPregunta) {
+    this.comentariosPregunta = comentariosPregunta;
+  }
+
+  public Pregunta getPreguntaPedida() {
+    return preguntaPedida;
+  }
+
+  public void setPreguntaPedida(Pregunta preguntaPedida) {
+    this.preguntaPedida = preguntaPedida;
+  }
+
+  public List<Pregunta> listarPreguntas() {
+    PreguntaDAO daoP = new PreguntaDAO();
+    List<Pregunta> preguntas = daoP.obtener(true, false);
+    return preguntas;
+  }
+
+  public String mostrarPregunta() {
+    int id = preguntaPedida.getIdPregunta();
+    if (id == 0) {
+      return "/index.xhtml?faces-redirect=true";
+    }
+    preguntaPedida = new PreguntaDAO().obtener(id, true, false);
+    comentariosPregunta = new ComentarioDAO().obtener(preguntaPedida, true);
+    return null;
+  }
 
 }
